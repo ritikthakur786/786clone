a21371ac373e719d4e6a7ebf8d4e0ae0247affa3
iamrkay786 <radhamusicbot5911@gmail.com>
2025-02-22 01:07:04 +0530

    Update help.py

--- PROMUSIC/cplugin/help.py
+++ PROMUSIC/cplugin/help.py
@@ -106,7 +106,7 @@ async def helper_cb(client, CallbackQuery, _):
     if cb == "hb9":
         if CallbackQuery.from_user.id not in SUDOERS:
             return await CallbackQuery.answer(
-                "ᴀʟᴘʜᴀ ᴋᴏ ᴘᴀᴘᴀ ʙᴏʟ ᴊᴀᴋᴇ ᴘᴀʜʟᴇ 😆😆", show_alert=True
+                "ꪜ 𝛊 ɭ ɭ ᧘ 𝛊 𝛈 ᴋᴏ ᴘᴀᴘᴀ ʙᴏʟ ᴊᴀᴋᴇ ᴘᴀʜʟᴇ 😆😆", show_alert=True
             )
         else:
             await CallbackQuery.edit_message_text(helpers.HELP_9, reply_markup=keyboard)
