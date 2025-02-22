0d38224f0426acd1d7017df97947ec4d21b41749
iamrkay786 <radhamusicbot5911@gmail.com>
2025-02-22 15:01:46 +0530

    Update ping.py

--- PROMUSIC/cplugin/ping.py
+++ PROMUSIC/cplugin/ping.py
@@ -46,7 +46,7 @@ async def ping_clone(client: Client, message: Message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("❄ sᴜᴘᴘᴏʀᴛ ❄", url="https://t.me/+gMy8Cp190ediNzZl"),
+                    InlineKeyboardButton("❄ sᴜᴘᴘᴏʀᴛ ❄", url="https://t.me/oldskoolgc"),
                     InlineKeyboardButton(
                         "✨ 𝙰𝙳𝙳 𝙼𝙴✨",
                         url=f"https://t.me/{bot.username}?startgroup=true",
