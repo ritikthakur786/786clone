dada1f0f3f286e79d97bf6a1a4ade0475b399c2b
iamrkay786 <radhamusicbot5911@gmail.com>
2025-02-22 13:40:55 +0530

    Update clone.py

--- PROMUSIC/plugins/tools/clone.py
+++ PROMUSIC/plugins/tools/clone.py
@@ -101,8 +101,8 @@ async def clone_txt(client, message, _):
                 "name": bot.first_name,
                 "token": bot_token,
                 "username": bot.username,
-                "channel": "PURVI_SUPPORT",
-                "support": "PURVI_UPDATES",
+                "channel": "iamvillain77",
+                "support": "oldskoolgc",
                 "premium" : False,
                 "Date" : False,
             }
