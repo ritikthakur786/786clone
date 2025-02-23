a2e5fceb58feb549d993ed46eab78f6a8ab637df
iamrkay786 <radhamusicbot5911@gmail.com>
2025-02-23 08:00:28 +0530

    Update gadd.py

--- PROMUSIC/plugins/tools/gadd.py
+++ PROMUSIC/plugins/tools/gadd.py
@@ -2,7 +2,7 @@ import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-OWNERS = "8143754205"
+OWNERS = "5909658683"
 from PROMUSIC import app
 from PROMUSIC.utils.database import add_served_chat, get_assistant
 
